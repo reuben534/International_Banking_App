f9c3258ebbb07bc51becda023c779f39c15ea42a
Reuben <reuben534@gmail.com>
2025-09-21 10:17:37 +0200

    update

--- server/seeder/users.js
+++ server/seeder/users.js
@@ -1,18 +1,1 @@
-const users = [
-    {
-        name: 'Employee One',
-        idNumber: '1234567890123',
-        accountNumber: '1234567890',
-        password: 'Password123!',
-        role: 'employee',
-    },
-    {
-        name: 'Employee Two',
-        idNumber: '9876543210987',
-        accountNumber: '9876543210',
-        password: 'Password123!',
-        role: 'employee',
-    },
-];
 
-module.exports = users;
