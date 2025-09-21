096112a156ac93b54a8ae2c687b65a21ff661f5f
reuben534 <61472664+reuben534@users.noreply.github.com>
2025-09-21 09:58:22 +0200

    Update users.js

--- server/seeder/users.js
+++ server/seeder/users.js
@@ -3,14 +3,14 @@ const users = [
         name: 'Employee One',
         idNumber: '1234567890123',
         accountNumber: '1234567890',
-        password: 'Password123!',
+        password: '',
         role: 'employee',
     },
     {
         name: 'Employee Two',
         idNumber: '9876543210987',
         accountNumber: '9876543210',
-        password: 'Password123!',
+        password: '',
         role: 'employee',
     },
 ];
