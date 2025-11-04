eda534f3aaae445aa43f32df107f6905d7342eb5
Reuben <reuben534@gmail.com>
2025-11-04 19:54:11 +0200

    update

---
+++ sonar-project.js
@@ -0,0 +1,16 @@
+const sonarqubeScanner = require('sonarqube-scanner');
+
+sonarqubeScanner({
+  serverUrl: process.env.SONAR_HOST_URL,
+  token: process.env.SONAR_TOKEN,
+  options: {
+    'sonar.projectKey': 'reuben534',
+    'sonar.projectName': 'reuben534/International_Banking_App',
+    'sonar.sources': 'client/src,server',
+    'sonar.exclusions': '**/node_modules/**,**/*.test.js',
+    'sonar.tests': 'client/src',
+    'sonar.test.inclusions': '**/*.test.js,**/*.test.jsx',
+    'sonar.javascript.lcov.reportPaths': 'coverage/lcov.info',
+    'sonar.language': 'js'
+  }
+}, () => {});
