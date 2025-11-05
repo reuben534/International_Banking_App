760be9d443ad6069b77f93bff6f8aab05e81996a
Reuben <reuben534@gmail.com>
2025-11-05 20:19:41 +0200

    update

--- server/server.js
+++ server/server.js
@@ -18,7 +18,11 @@ connectDB();
 const app = express();
 app.set('trust proxy', 1);
 
-app.use(cors());
+// In production, you should set this to your client's URL
+const corsOptions = {
+    origin: process.env.NODE_ENV === 'production' ? 'YOUR_PRODUCTION_URL' : 'http://localhost:3000',
+};
+app.use(cors(corsOptions));
 app.use(helmet());
 app.use(express.json());
 app.use(mongoSanitize());
@@ -50,8 +54,12 @@ app.use('/api/employees', employeeRoutes);
 
 // Error handling middleware
 app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send('Something broke!');
+    const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+    res.status(statusCode);
+    res.json({
+        message: err.message,
+        stack: process.env.NODE_ENV === 'production' ? null : err.stack,
+    });
 });
 
 const PORT = process.env.PORT || 5000;
@@ -60,10 +68,8 @@ const https = require('node:https');
 const fs = require('node:fs');
 
 const options = {
-    key: fs.readFileSync(__dirname + '/key.pem'),
-    cert: fs.readFileSync(__dirname + '/cert.pem'),
+    key: fs.readFileSync(process.env.KEY_PATH),
+    cert: fs.readFileSync(process.env.CERT_PATH),
 };
 
-https.createServer(options, app).listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+https.createServer(options, app).listen(PORT);
