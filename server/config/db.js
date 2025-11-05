fde936384d88fd21936550ddc293f2616c5c6fc8
Reuben <reuben534@gmail.com>
2025-11-05 21:09:44 +0200

    update

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI);
+        await mongoose.connect(process.env.MONGO_URI);
     } catch (error) {
         console.error(`Error: ${error.message}`);
         process.exit(1);
