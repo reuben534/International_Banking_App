9d2c054afea4604c0f19d014759a1f09f3296559
Reuben <reuben534@gmail.com>
2025-11-07 12:15:02 +0200

    update

--- client/src/screens/LoginScreen.js
+++ client/src/screens/LoginScreen.js
@@ -42,7 +42,7 @@ const LoginScreen = () => {
       newErrors.accountNumber = 'Account Number must be 10 digits';
     }
     if (!password) {
-      newErrors.password = 'This field is required';
+      newErrors.password = 'password is required';
     }
     setErrors(newErrors);
     return Object.keys(newErrors).length === 0;
